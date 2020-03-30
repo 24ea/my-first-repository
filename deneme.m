f349e5d133858cccfe28c20528349f99a394bc36
24ea <62866219+24ea@users.noreply.github.com>
2020-03-30 17:00:56 +1300

    Added new section

--- deneme.m
+++ deneme.m
@@ -3,3 +3,7 @@ A Youtube channel called The Coding Train seems to be helpful.
 
 Following the instructions on the first video about Git & Github - for Rainbow Poet.
 
+%% Second Part
+NowLearningToEditFile = true;
+
+
