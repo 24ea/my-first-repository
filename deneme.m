0a02a5bf73cbb6dba77da2310d424b0441e979c6
24ea <62866219+24ea@users.noreply.github.com>
2020-03-30 17:04:31 +1300

    text added in the middle

--- deneme.m
+++ deneme.m
@@ -3,6 +3,9 @@ A Youtube channel called The Coding Train seems to be helpful.
 
 Following the instructions on the first video about Git & Github - for Rainbow Poet.
 
+%% A new edit in the middle
+SoWhatsNext;
+
 %% Second Part
 NowLearningToEditFile = true;
 
